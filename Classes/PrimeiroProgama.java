c655cf73c933978f563d07412d771b21ea565ed2
ZaccMarc <isaacfm7077@gmail.com>
2025-05-26 20:50:18 -0300

    Enviando projeto de aulas de Java

---
+++ Classes/PrimeiroProgama.java
@@ -0,0 +1,5 @@
+public class PrimeiroProgama {
+    public static void main(String[] args){
+        System.out.print("Olá Mundo"); 
+    }
+}
---
+++ POO/Main.java
@@ -0,0 +1,12 @@
+package POO;
+
+public class Main {
+    public static void main(String[] args) {
+        // Instanciando objetos
+        ControleRemoto controle = new ControleRemoto();
+        // Testando o controle remoto
+        controle.ligar();
+        controle.abrirMenu();
+    }
+}
+  
